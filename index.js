16e4ccad17606f4c420d540fdad1e6120e96bf54
devgirlpro <agalidari@gmail.com>
2024-02-27 10:07:59 +0100

    add another item for stor

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const createStore = redux.createStore;
 const CAKE_ORDERED = 'CAKE_ORDERD';
 const CAKE_RESTOCKED = "CAKE_RESTOCKED"
 
+const ICECREAM_ORDERED = 'ICECREAM_ORDERED';
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED"
 
-//action creator function
+
+//action creator function for order cake
 function orderCake() {
     return {
         type: CAKE_ORDERED,
@@ -13,17 +16,36 @@ function orderCake() {
     }
 }
 
+
+
 //action creator for restocked cake
-function restockedCake(qty = 5) {
+function restockedCake(qty = number) {
     return {
         type: CAKE_RESTOCKED,
         payload: qty
     }
 }
 
+//action creator for order icecream
+function orderIceCream(qty = 1) {
+    return {
+        type: ICECREAM_ORDERED,
+        payload: qty
+    }
+}
+
+//action creator for restoked icecream
+function restockedIceCream(qty = number) {
+    return {
+        type: ICECREAM_RESTOCKED,
+        payload: qty,
+    }
+}
+
 //state => has to be a single obgect
 const initialState = {
     numberOfCakes: 10,
+    numberOfIceCreams: 15,
 }
 
 //(previousState, action) => newState
@@ -39,6 +61,17 @@ const reducer = (state = initialState, action) => {
                     ...state,
                     numberOfCakes: state.numberOfCakes + action.payload
                 }
+            case ICECREAM_ORDERED:
+                return {
+                    ...state,
+                    numberOfIceCreams: state.numberOfIceCreams - 1
+                }
+            case ICECREAM_RESTOCKED:
+                return {
+                    ...state,
+                    numberOfIceCreams: state.numberOfIceCreams + action.payload
+                }
+                
             default:
                 return state
     }
@@ -59,6 +92,14 @@ store.dispatch(orderCake())
 store.dispatch(orderCake())
 store.dispatch(orderCake())
 store.dispatch(restockedCake(5))
+
+//useing bindActionCreators
+const actions = redux.bindActionCreators({restockedIceCream, orderIceCream}, store.dispatch)
+
+actions.orderIceCream()
+actions.orderIceCream()
+
+actions.restockedIceCream(5)
 
 
 //unsubscribe from the store: by calling the function returned by the subscribe mehod
